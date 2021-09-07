2a9964441676255381b281c15b693e799a51e9ed
LeonBaram <leonbaram@protonmail.com>
2021-09-07 19:34:26 -0400

    implement elementalWords algorithm (first pass)

--- src/utils/elementify.ts
+++ src/utils/elementify.ts
@@ -1,3 +1,39 @@
+const elements = {};
+
 export function elementify(word: string): string[][] {
-  return [];
+  const output: string[][] = [];
+  const stack: [string[], string][] = [[[], word]];
+
+  while (stack) {
+    const [parents, word] = stack.pop();
+
+    for (let element in elements) {
+      if (word.startsWith(element)) {
+        stack.push([[...parents, element], word.slice(element.length)]);
+      }
+    }
+
+    if (word.length === 0) {
+      output.push(parents);
+    }
+  }
+
+  return output;
 }
+
+/*
+original implementation:
+
+def elemental_forms(word):
+    result = []
+    stack = [ ([], word.capitalize()) ]
+    while stack:
+        parents, word = stack.pop()
+        if not word:
+            result.append(parents)
+            continue
+        for element, name in ELEMENTS.items():
+            if word.startswith(element):
+                stack.append((parents + [f'{name} ({element})'], word[len(element):].capitalize()))
+    return result
+*/
